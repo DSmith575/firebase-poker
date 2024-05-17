dd632ddf6e53ff23c73f9dc479942a69c83872c9
Deacon Smith (1000106517) <SMITDE5@student.op.ac.nz>
2024-05-17 12:36:13 +1200

    build(component): added prettier

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDvVTxJZ_lLTGhmhsK1bbqPp8XTIKWckL8",
-  authDomain: "adv-app-dev-assignment.firebaseapp.com",
-  projectId: "adv-app-dev-assignment",
-  storageBucket: "adv-app-dev-assignment.appspot.com",
-  messagingSenderId: "923720245338",
-  appId: "1:923720245338:web:1dd5e75ae306ede64c3863"
+  apiKey: 'AIzaSyDvVTxJZ_lLTGhmhsK1bbqPp8XTIKWckL8',
+  authDomain: 'adv-app-dev-assignment.firebaseapp.com',
+  projectId: 'adv-app-dev-assignment',
+  storageBucket: 'adv-app-dev-assignment.appspot.com',
+  messagingSenderId: '923720245338',
+  appId: '1:923720245338:web:1dd5e75ae306ede64c3863',
 };
 
 // Initialize Firebase
