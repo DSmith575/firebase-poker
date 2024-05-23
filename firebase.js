8065d6e006114ad755f1431c22d830f0ef2f364c
DSmith575 <SMITDE5@student.op.ac.nz>
2024-05-23 17:57:45 +1200

    feat(setup): adding firebase config files

--- firebase.js
+++ firebase.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyDvVTxJZ_lLTGhmhsK1bbqPp8XTIKWckL8',
   authDomain: 'adv-app-dev-assignment.firebaseapp.com',
@@ -14,7 +11,14 @@ const firebaseConfig = {
   appId: '1:923720245338:web:1dd5e75ae306ede64c3863',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+const firebaseAuth = getAuth(app);
+
+initializeFirestore(app, {
+  localCache: persistentLocalCache({ tabManager: persistentMultipleTabManager() }),
+});
+
+const db = getFirestore(app);
+
+export { firebaseAuth, db };
