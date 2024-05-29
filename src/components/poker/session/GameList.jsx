a82a11d34683ec8436ed4a102877b0b641411adb
Deacon Smith (1000106517) <SMITDE5@student.op.ac.nz>
2024-05-29 17:12:26 +1200

    feat(component): made changes

--- src/components/poker/session/GameList.jsx
+++ src/components/poker/session/GameList.jsx
@@ -3,6 +3,8 @@ import useGameList from '../../../hooks/games/useGameList';
 import JoinGame from './JoinGame';
 import { ImSpinner2 } from 'react-icons/im';
 import InputField from '../../input/Input';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 const GameList = () => {
   const [filter, setFilter] = useState(false);
@@ -24,17 +26,24 @@ const GameList = () => {
         </div>
         <div className="">
           {!loading('gameList') ? (
-            <div className="grid sm:grid-cols-2 lg:grid-cols-3">
+            <Swiper
+              spaceBetween={25}
+              slidesPerView={2}
+              autoplay={true}
+              onSlideChange={() => console.log('slide change')}
+              onSwiper={(swiper) => console.log(swiper)}>
               {games.map((game) => (
-                <div
-                  key={game.id}
-                  className="text-center md:rounded-xl my-2 mx-2 bg-slate-500 transition duration-700 ease-in-out hover:bg-sky-500">
-                  <h2>{game.gameName}</h2>
-                  <p>Player Limit: {game.totalPlayers}</p>
-                  <JoinGame gameId={game.id} gameStarted={game.owner} />
-                </div>
+                <SwiperSlide key={game.id}>
+                  <div
+                    key={game.id}
+                    className="text-center md:rounded-xl my-2 mx-2 bg-slate-500 transition duration-700 ease-in-out hover:bg-sky-500">
+                    <h2>{game.gameName}</h2>
+                    <p>Player Limit: {game.totalPlayers}</p>
+                    <JoinGame gameId={game.id} gameStarted={game.owner} />
+                  </div>
+                </SwiperSlide>
               ))}
-            </div>
+            </Swiper>
           ) : (
             <>
               <div className="flex items-center justify-center z-50">
