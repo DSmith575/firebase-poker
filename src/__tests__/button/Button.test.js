efbe9b8227a8820cc16fcadc476330f126528fd3
Deacon Smith (1000106517) <SMITDE5@student.op.ac.nz>
2024-06-01 13:42:05 +1200

    test(button): added tests for Button component

---
+++ src/__tests__/button/Button.test.js
@@ -0,0 +1,42 @@
+import Button from '../../components/button/Button';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('Button', () => {
+  test('should render Button component', () => {
+    render(<Button label={'Test Button'} />);
+    const button = screen.getByText('Test Button');
+
+    expect(button).toBeInTheDocument();
+  });
+
+  test('should render Button component with disabled attribute', () => {
+    render(<Button label={'Test Button'} disabled={true} />);
+    const button = screen.getByText('Test Button');
+
+    expect(button).toBeDisabled();
+  });
+
+  test('should render Button component with value attribute', () => {
+    render(<Button label={'Test Button'} type={'button'} value={'test'} />);
+    const button = screen.getByText('Test Button');
+
+    expect(button).toHaveAttribute('value', 'test');
+  });
+
+  test('should render Button component with type attribute', () => {
+    render(<Button label={'Test Button'} type={'submit'} />);
+    const button = screen.getByText('Test Button');
+
+    expect(button).toHaveAttribute('type', 'submit');
+  });
+
+  test('should call onClick function when button is clicked', () => {
+    const onClick = jest.fn();
+    render(<Button label={'Test Button'} onClick={onClick} />);
+    const button = screen.getByText('Test Button');
+
+    button.click();
+    expect(onClick).toHaveBeenCalled();
+  });
+});
