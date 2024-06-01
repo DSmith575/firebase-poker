abf8f2d9923218950dee86cf238aa406a72e916e
Deacon Smith (1000106517) <SMITDE5@student.op.ac.nz>
2024-06-01 14:24:52 +1200

    test(randomcolors): added test for util getRandomColors

--- src/router/routerList.jsx
+++ src/router/routerList.jsx
@@ -24,6 +24,11 @@ const routes = [
     label: 'Sign out',
     element: <SignOut />,
   },
+  {
+    path: '/currentGame/:id',
+    label: 'Current Game',
+    element: '<GamePage />',
+  },
 ];
 
 export default routes;
